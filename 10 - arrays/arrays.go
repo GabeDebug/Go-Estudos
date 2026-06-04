f76f297a13b85065741a1c7cc363e24d4c8db29b
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-06-04 10:19:29 -0300

    loops

--- 10 - arrays/arrays.go
+++ 10 - arrays/arrays.go
@@ -5,10 +5,14 @@ import "fmt"
 func main() {
 	arr := [3]int{10, 20, 30}
 	arr2 := [10]int{1: 100, 9: 200}
+	// eu posso fazer todo tipo de array
+
+	nomes := [5]string{1: "gabriel", 0: "davi"}
+	fmt.Println(nomes)
 
 	fmt.Println(arr2)
 
-	// indices
+	// usando array com indices
 	arr[2] = 100
 	fmt.Println(arr)
 }
