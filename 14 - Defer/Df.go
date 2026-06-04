56e63d8f875c20b5ee72982161f5efa16de8d896
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-06-04 21:53:11 +0000

    Defer executa ultimo

--- 14 - Defer/Df.go
+++ 14 - Defer/Df.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	/*
+	 O defer no Golang (Go)
+	 é uma palavra-chave usada para adiar a execução de uma
+	 função até o final da função atual.
+	*/
+	fmt.Println("Inicio")
+
+	defer fmt.Println("Isso roda por ultimo")
+	// usando o defer vai fazer a função
+	// ser executada por ultimo
+
+	fmt.Println("fim")
+
+	// o defer sempre executa depois de tudo, no final da
+	// função
+
+	// Ordem de execução (muito importante)
+
+	/*
+		Se você usar vários defer, eles funcionam como uma
+		pilha (LIFO = último que entra, primeiro que sai):
+	*/
+
+	defer fmt.Println("1") // terceiro
+	defer fmt.Println("2") // segundo
+	defer fmt.Println("3") // vai ser primeiro
 }
