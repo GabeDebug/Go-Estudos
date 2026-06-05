602d524b2d66c709d06a0b40e694bd81b5d22f7b
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-06-04 22:06:58 -0300

    Defer

--- 14 - Defer/Df.go
+++ 14 - Defer/Df.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("Inicio")
+
+	defer fmt.Println("vai ser executado no final")
+	/*
+		Quando usamos o defer a função que estiver nela
+		vai ser executada por ultimo
+	*/
+
+	fmt.Println("Vai ser executado em segundo")
 }
