727c44e8bdcfc50d23849e1798b0b413f184a5af
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-06-05 21:01:17 -0300

    Função avançada

--- 18 - função avançado/18.2 - função Variáticas/variaticas.go
+++ 18 - função avançado/18.2 - função Variáticas/variaticas.go
@@ -12,7 +12,21 @@ func soma(numeros ...int) int {
 	return total
 }
 
+func escrever(texto string, numeros ...int) {
+	for _, numero := range numeros {
+		fmt.Println(texto, numero)
+	}
+}
+
+func Escrever(texto string, numeros ...int) {
+	for _, numero := range numeros {
+		fmt.Println(texto, numero)
+	}
+}
+
 func main() {
+	Escrever("Estudando Golang", 1, 24, 1, 24, 12, 31, 25, 12, 3)
 	totalDaSoma := soma(1, 2, 4, 5, 1, 5, 12, 3, 41)
 	fmt.Println(totalDaSoma)
+	escrever("Hello World", 1, 1, 1, 23, 5, 123, 1)
 }
