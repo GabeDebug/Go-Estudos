324c293283f9302b908613aa10d5bb22dffbb447
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-06-08 23:57:57 -0300

    interfaces

--- 1 - Pacotes/auxiliar/auxiliar.go
+++ 1 - Pacotes/auxiliar/auxiliar.go
@@ -9,7 +9,7 @@ func Escrever() {
 	fmt.Println("Escrevendo do pacote auxiliar")
 	escrever2()
 }
+
 // se uma função for com letra
 // minuscula ela só vai ser vista no pacote que ela está
 // com letra maiuscula ela vai ser exportada para outros pacotes
-
